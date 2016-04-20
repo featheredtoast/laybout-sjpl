630a2204c1d6849e73d7e163b11d98b5a712c35a
Jeff Wong <awole20@gmail.com>
2016-04-19 18:34:25 -0700

    making some cards

--- src/cljs/sjlibrary/core.cljs
+++ src/cljs/sjlibrary/core.cljs
@@ -9,27 +9,33 @@
   [:div {:class "app"}
    [:div {:class "row"}
     [:div {:class "col-md-4"}
-     "TV's Adam west to visit the SJPL!!!"]
+     [:div {:class "card"}
+      "TV's Adam west to visit the SJPL!!!"]]
     [:div {:class "col-md-4"}
-     "New NASA Shuttle exibit 2018!"]
+     [:div {:class "card"}
+      "New NASA Shuttle exibit 2018!"]]
     [:div {:class "col-md-4"}
-     "Things people write!"
-     [:ul
-      [:li "Feelings!"]
-      [:li "Thoughts!"]
-      [:li "Deep philosophical ramblings!"]
-      [:li "Other things!"]
-      [:li "More other things!"]]]]
+     [:div {:class "card"}
+      "Things people write!"
+      [:ul
+       [:li "Feelings!"]
+       [:li "Thoughts!"]
+       [:li "Deep philosophical ramblings!"]
+       [:li "Other things!"]
+       [:li "More other things!"]]]]]
    [:div {:class "row"}
     [:div {:class "col-md-4"}
-     "Build your own Iron Man suit! (Core reactor not included)"]
+     [:div {:class "card"}
+      "Build your own Iron Man suit! (Core reactor not included)"]]
     [:div {:class "col-md-4"}
-     "SJPL celebrates national Pet A Cat Day with ALL THE CATS!"]
+     [:div {:class "card"}
+      "SJPL celebrates national Pet A Cat Day with ALL THE CATS!"]]
     [:div {:class "col-md-4"}
-     "Services"
-     [:ul
-     [:li "Books!"]
-     [:li "Movies!"]
-     [:li "Homework help! (Passing grade not guaranteed"]]]]])
+     [:div {:class "card"}
+      "Services"
+      [:ul
+       [:li "Books!"]
+       [:li "Movies!"]
+       [:li "Homework help! (Passing grade not guaranteed)"]]]]]])
 
 (reagent/render [app] (js/document.getElementById "app"))
